d17e0abbfe0018c0843ac527a441b471ddc41557
mmartinez263 <31322029+mmartinez263@users.noreply.github.com>
2018-10-09 12:20:01 -0700

    Change 2 completed

--- Main.java
+++ Main.java
@@ -31,14 +31,27 @@ class Main
          */
         System.out.println("*** PRINTING ar1 *");
         int[] ar1 = new int[n];
+
         for (int i=0; i<n; i++)
             ar1[i]=i;
-        
-                
+        for (int i=0 ; i<n; i++)
+            System.out.println("ar1[" + i + "] = "+ar1[i]);        
         /*
          * Task 2.  Create a new array called ar2.
          *          Copy the elements of ar1 into ar2.
          */
+        //This clones ar1[] into ar2
+        //int[] ar2 = ar1.clone();    
+        System.out.println("** ar2 **");
+        int[] ar2 = new int[ar1.length];
+        for(int i=0 ; i<ar2.length ; i++)
+            ar2[i]=ar1[i];
+        
+                       
+
+        
+        for (int i=0 ; i<ar2.length ; i++)
+            System.out.println("ar2[" + i + "] = "+ar2[i]);
         
         
         /* 
